db55d7a22e1b904be95509c1c736607e4292d715
kia-h <mhey237@gmail.com>
2016-10-22 15:32:06 +1100

    Add other changes to studen model + full name

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,15 +11,31 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
 
+        [Required]
         [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Required]
         [StringLength(50,ErrorMessage = "First Name cannot be longer than 50 characters")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollement Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + " " + FirstMidName;
+                
+            }
+        }
         public virtual ICollection<Enrollment>  Enrollments { get; set; }
     }
 }
